030a08dcd973c42dc09bc98930d52c51feb4d577
Jose C Ponce Diaz <pepote5391@gmail.com>
2018-08-23 21:41:50 -0400

    initial commit

---
+++ src/main/java/com/example/spark/spring/Entity.java
@@ -0,0 +1,14 @@
+package com.example.spark.spring;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Entity implements Serializable {
+    private String value;
+}
