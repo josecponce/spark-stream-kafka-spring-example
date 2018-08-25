df6e8156982dbd1019d06ca88003b31c126babaf
Jose C Ponce Diaz <pepote5391@gmail.com>
2018-08-24 22:46:38 -0400

    fixing silly issue

--- src/main/scala/com/example/spark/spring/Application.scala
+++ src/main/scala/com/example/spark/spring/Application.scala
@@ -3,7 +3,7 @@ package com.example.spark.spring
 import java.util.Properties
 
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.{SQLContext, SaveMode, SparkSession}
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.InputDStream
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -13,15 +13,14 @@ import org.springframework.context.annotation.Bean
 @SpringBootApplication
 class Application {
   @Bean
-  def runner(spark: SparkSession, scc: StreamingContext,
+  def runner(sqlContext: SQLContext, spark: SparkSession, scc: StreamingContext,
              stream: InputDStream[ConsumerRecord[String, String]]): ApplicationRunner = new ApplicationRunner {
     override def run(args: ApplicationArguments): Unit = {
       stream.foreachRDD((rdd, time) => {
         val connectionProps = new Properties()
         connectionProps.setProperty("user", "root")
         connectionProps.setProperty("password", "password")
-        val dataFrame = spark.createDataFrame(
-          rdd.map(record => Entity(record.value())), classOf[Entity])
+        val dataFrame = spark.createDataFrame(rdd.map[Entity](record => Entity(record.value())))
         dataFrame.write.mode(SaveMode.Append).jdbc("jdbc:mysql://node-3/spark", "test", connectionProps)
       })
       scc.start()
